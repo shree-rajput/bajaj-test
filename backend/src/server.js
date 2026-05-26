cc3bec8092cd62282b570c5506d41401c407db50
shree rajput <shreerajputt4509@gmail.com>
2026-05-26 16:25:44 +0530

    finally

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 const app = require('./app');
 const connectDB = require('./config/db');
-
+const cors = require('cors');
 // Connect to the database
 connectDB();
 
